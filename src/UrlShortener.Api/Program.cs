fad3528c43538938e3c25f3b9b5c54b11c9508fd
Daniel Viola <daniel.viola001@mymdc.net>
2026-08-22 08:46:00 -0400

    a

--- src/UrlShortener.Api/Program.cs
+++ src/UrlShortener.Api/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using UrlShortener.Api.Data;
+using UrlShortener.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +8,24 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseNpgsql(
         builder.Configuration.GetConnectionString("Postgres"));
-
 });
 
 builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/health", () => Results.Ok(new
+{
+    status = "healthy"
+}));
+
+app.MapUrlEndpoints();
+
+app.Run();
